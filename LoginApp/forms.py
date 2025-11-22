c025fa54d85315a4c6e8ca063db3a0e39abc97db
vxtxcx <victorvicencio932@gmail.com>
2025-11-22 11:43:09 -0300

    Login completo
    se completo el login aplicando css dandole un mejor diseño

--- LoginApp/forms.py
+++ LoginApp/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    
+
     UsernameField = forms.CharField(
-        label='Nombre de usuario'
+        label='Nombre de usuario',
+        error_messages={
+            'required': 'Este campo no puede quedar vacío.'
+        }
     )
 
     PasswordField = forms.CharField(
-        label='Contraseña'
+        label='Contraseña',
+        widget=forms.PasswordInput, 
+        error_messages={
+            'required': 'Este campo no puede quedar vacío.'
+        }
     )
 
     class Meta:
